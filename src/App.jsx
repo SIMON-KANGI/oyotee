32644f7ffbc4536a18560778ab7b5343b3bfa72e
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-17 10:34:07 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import NavBar from "./components'/Navbar"
-import Home from "./pages.jsx/Home"
+import Home from "./pages/Home"
 import { Routes,Route } from "react-router-dom"
+import Cart from "./pages/Cart"
 function App(){
   return(
     <>
 <NavBar/>
 <Routes>
   <Route path="/" element={<Home />} />
-  
+  <Route path="/cart" element={<Cart/>} />
 </Routes>
     </>
   )
