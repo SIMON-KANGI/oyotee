7f58d6a27113e5d1f2ceb766698fc42490087b52
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-17 10:34:24 +0300

    added store

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import store from './features/Store.jsx'
+import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Provider store={store}>
   <ChakraProvider>
   <BrowserRouter>
       <App />
       </BrowserRouter>
   </ChakraProvider>
-  
+  </Provider>
   </StrictMode>,
 )
